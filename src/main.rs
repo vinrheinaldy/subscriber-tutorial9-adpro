155fbfc4b6e4f3c08125f8160bca6f002e010884
Alvin <imoonfellow@gmail.com>
2025-05-16 13:28:50 +0700

    intial subscriber code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use crosstown_bus::{CrosstownBus, MessageHandler, HandleError};
+use std::{thread, time};
+
+#[derive(Debug, Clone, BorshDeserialize, BorshSerialize)]
+pub struct UserCreatedEventMessage {
+    pub user_id: String,
+    pub user_name: String,
+}
+
+pub struct UserCreatedHandler;
+
+impl MessageHandler<UserCreatedEventMessage> for UserCreatedHandler {
+    fn handle(&self, message: Box<UserCreatedEventMessage>) -> Result<(), HandleError> {
+        let ten_millis = time::Duration::from_millis(1000);
+        let now = time::Instant::now();
+
+        // thread::sleep(ten_millis);
+
+        println!("In Alvin's computer 2306275866. Message received: {:?}", message);
+        Ok(())
+    }
+    // error otherwise
+    fn get_handler_action(&self) -> String {
+        "user_created".to_string()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let listener = CrosstownBus::new_queue_listener("amqp://guest:guest@localhost:5672".to_owned()).unwrap();
+    _ = listener.listen("user_created".to_owned(), UserCreatedHandler{}, crosstown_bus::QueueProperties {
+        auto_delete: false,
+        durable: false,
+        use_dead_letter: true
+    });
+
+    loop {}
 }
